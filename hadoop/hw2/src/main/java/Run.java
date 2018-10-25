9345d238171d1a999d70a6da4b178632879f72fb
Kakoedlinnoeslovo <roma-degtayrev@mail.ru>
2018-10-25 21:04:39 +0300

    изменил Run

--- hadoop/hw2/src/main/java/Run.java
+++ hadoop/hw2/src/main/java/Run.java
@@ -1,12 +1,16 @@
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
+
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.mapred.JobConf;
 
 
 import javax.xml.soap.Text;
@@ -24,7 +28,11 @@ public class Run extends Configured implements Tool {
         job.setJarByClass(Run.class);
         job.setJobName(Run.class.getCanonicalName());
 
-        FileInputFormat.setInputPaths(job, new Path(input));
+        Path inputPath = new Path(input);
+
+        MultipleInputs.addInputPath(job, inputPath,
+                TextInputFormat.class, MapperClass.class);
+
         FileOutputFormat.setOutputPath(job, new Path(output));
 
         job.setMapperClass(MapperClass.class);
