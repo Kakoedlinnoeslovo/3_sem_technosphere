b9f4aaf6aab004410c51a9631d095e0de033474e
Kakoedlinnoeslovo <roma-degtayrev@mail.ru>
2018-10-26 16:50:23 +0300

    add regex

--- hadoop/hw2/src/main/java/getLinks.java
+++ hadoop/hw2/src/main/java/getLinks.java
@@ -5,13 +5,15 @@ import java.util.zip.Inflater;
 import java.util.LinkedList;
 import java.io.ByteArrayOutputStream;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 class getLinks {
     private static final Logger LOG = Logger.getLogger(getLinks.class.getName());
 
      static LinkedList<String> extract (String data) throws IOException {
 
-        LinkedList result  = new LinkedList<String>();
+        LinkedList <String> links  = new LinkedList<>();
         byte[] decoded = Base64.getDecoder().decode(data);
 
         Inflater inflater = new Inflater();
@@ -34,12 +36,18 @@ class getLinks {
 
         //System.out.println("Original size: " + data.length()/1024 + "KB");
         //System.out.println ("Uncompressed size: " + HTML.length()/1024 + "KB");
-         System.out.println(HTML);
+         //System.out.println(HTML);
+
+         Pattern linkPattern = Pattern.compile("<a\\s+(?:[^>]*?\\s+)?href=([\"'])(.*?)\\1");
+         Matcher matcher = linkPattern.matcher(HTML);
+         while (matcher.find()){
+             links.add(matcher.group(2));
+         }
 
 
+         System.out.println(links.toString());
 
-
-        return result;
+        return links;
 
     }
 
