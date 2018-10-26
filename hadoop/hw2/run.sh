d9ebda1e362a08f17b7808755087562204c7c010
Roman <roma-degtayrev@mail.ru>
2018-10-26 14:56:04 +0300

    change

--- hadoop/hw2/run.sh
+++ hadoop/hw2/run.sh
@@ -1,8 +1,4 @@
 hadoop dfs -rm -r out
 git pull
 gradle jar
-<<<<<<< HEAD
 hadoop jar build/libs/hw2.jar "$@" "$@" "$@"
-=======
-hadoop jar build/libs/hw2.jar UrlsToFile "$@" "$@"
->>>>>>> 92a25a0735fed5b30d65172b11018a8b47a61cfd
