48eaaad2d9fb28fa223bb83563c8e1f3df9c0b79
Kakoedlinnoeslovo <roma-degtayrev@mail.ru>
2018-10-29 12:37:48 +0300

    init mapper of HitsGetTop

--- hadoop/hw2/src/main/java/HitsGetTop.java
+++ hadoop/hw2/src/main/java/HitsGetTop.java
@@ -12,15 +12,30 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 public class HitsGetTop extends Configured implements Tool {
 
+    LinkedList <String> from = new LinkedList<>();
+    LinkedList <String> to = new LinkedList<>();
+    private static int FIRST_START = 0;
 
     static public class HitsgetTopMapper extends Mapper <LongWritable, Text, Text, Text>{
         @Override
         public final void map(LongWritable offset, Text data, Context context) throws IOException{
 
-            
+            if (FIRST_START == 1){
+                String dataStr = data.toString();
+                String[] dataArray = dataStr.split("\t");
+                for (String d: dataArray){
+                    if (d.substring(0, 2).equals("|F|")){
+                        System.out.println("FROM URLS is " + d.substring(2));
+                    }
+                    if (d.substring(0, 2).equals("|T|")){
+                        System.out.println("TO URLS is " + d.substring(2));
+                    }
+                }
+            }
 
         }
 
@@ -37,16 +52,22 @@ public class HitsGetTop extends Configured implements Tool {
 
 
 
-    private static Job GetJobConf(Configuration conf) throws IOException {
+    private static Job GetJobConf(Configuration conf, String[] args) throws IOException {
         Job job = Job.getInstance(conf);
         job.setJarByClass(HitsGetTop.class);
         job.setJobName(HitsGetTop.class.getCanonicalName());
 
-        Path inputPath = Constants.HitsOutputPath;
+        Path inputPath = new Path(args[0]);
+        if (new Path(args[0]) == Constants.HitsOutputPath){
+            FIRST_START = 1;
+        }
+        else{
+            FIRST_START = 0;
+        }
         Path outputPath = Constants.HitsGetTopOutputPath;
 
         job.setMapperClass(HitsgetTopMapper.class);
-        job.setReducerClass(HitsgetTopReducer.class);
+        //job.setReducerClass(HitsgetTopReducer.class);
 
         FileInputFormat.addInputPath(job, inputPath);
         FileOutputFormat.setOutputPath(job, outputPath);
@@ -62,7 +83,7 @@ public class HitsGetTop extends Configured implements Tool {
 
     @Override
     public int run(String[] args) throws Exception{
-        Job job = GetJobConf(getConf());
+        Job job = GetJobConf(getConf(), args);
         return job.waitForCompletion(true) ? 0 : 1;
     }
 
