35600a9b53280b4bc9b98de5eac05597b13a97e9
Kakoedlinnoeslovo <roma-degtayrev@mail.ru>
2018-10-29 10:39:43 +0300

    fix

--- hadoop/hw2/src/main/java/HistsGetTop.java
+++ hadoop/hw2/src/main/java/HistsGetTop.java
@@ -1,18 +1,18 @@
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.fs.Path;
-
-import java.io.IOException;
-
-public class HistsGetTop {
-
-
-
-    public static Job GetJobConf(Configuration conf) throws IOException {
-        Job job = Job.getInstance(conf);
-        job.setJarByClass(HistsGetTop.class);
-        job.setJobName(HistsGetTop.class.getCanonicalName());
-
-        return job;
-    }
-}
+//import org.apache.hadoop.conf.Configuration;
+//import org.apache.hadoop.mapreduce.Job;
+//import org.apache.hadoop.fs.Path;
+//
+//import java.io.IOException;
+//
+//public class HistsGetTop {
+//
+//
+//
+//    public static Job GetJobConf(Configuration conf) throws IOException {
+//        Job job = Job.getInstance(conf);
+//        job.setJarByClass(HistsGetTop.class);
+//        job.setJobName(HistsGetTop.class.getCanonicalName());
+//
+//        return job;
+//    }
+//}
