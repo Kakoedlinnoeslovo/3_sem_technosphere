5f33abef9a1720c11fabec278cb085feb012c1df
Kakoedlinnoeslovo <roma-degtayrev@mail.ru>
2018-11-01 02:38:36 +0300

    fix

--- hadoop/hw3/run.sh
+++ hadoop/hw3/run.sh
@@ -3,32 +3,29 @@
 set -e
 set -o pipefail
 
-Log(){
-    echo "[$( date + %c)] $*" >&2
+Log() {
+    echo "[$( date +%c )] $*" >&2
 }
 
-OUTDIR = hw3/out
-RESFILE = temps.kml
-NREDUCERS = 4
+OUTDIR=seminar3/out
+RESFILE=temps.kml
+NREDUCERS=4
 
-Lof "BUILDING"
+Log "BUILDING"
 ./gradlew jar
 
-Log "REMOVING previous directory"
 
+
+Log "REMOVING previous directory"
 hadoop fs -rm -r -f $OUTDIR
 
-
 Log "RUNNING JOB"
+hadoop jar ./build/libs/seminar3.jar SecondarySortDemo \
+           -Dmapreduce.job.reduces=$NREDUCERS \
+           /data/seminar2/meteo/*.gz $OUTDIR
 
-hadoop jar ./build/libs/hw3.jar SecondarySortDemo \
-            -Dmapreduce.job.reduces  = $NREDUCRERS \
-            /data/seminar2/meteo/*.gz $OUTDIR
-
-hadoop fs -text "$OUTDIR/part-r*" | fgrep "$( date + %d.%m)" | .ids2coords.py > temps.csv
-gpsbabel -i csv -f temps.csv -o km; -F $RESFILE
+hadoop fs -text "$OUTDIR/part-r*" | fgrep "$( date +%d.%m )" | ./ids2coords.py >temps.csv
+gpsbabel -i csv -f temps.csv -o kml -F $RESFILE
 
 Log "SUCCESS. Result in temps.kml"
-
-
 
