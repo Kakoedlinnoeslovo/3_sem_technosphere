358cbaa6c8515f8d28f2c10b81347f653208aff0
Kakoedlinnoeslovo <roma-degtayrev@mail.ru>
2018-11-04 19:22:23 +0300

    change

--- hadoop/hw3/src/main/java/SeoSortJob.java
+++ hadoop/hw3/src/main/java/SeoSortJob.java
@@ -20,7 +20,7 @@ public class SeoSortJob extends Configured implements Tool {
 
     public static int getMinClicks(Configuration conf)
     {
-        return conf.getInt("seo.minclicks", 1);
+        return conf.getInt("minclicks", 1);
     }
 
     @Override
